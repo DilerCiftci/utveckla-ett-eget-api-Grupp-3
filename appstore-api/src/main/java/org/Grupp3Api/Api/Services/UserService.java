43a4a75338a4f7ac5fae0b87178f7cd155701843
Jonatan Andersson <Jonatan.anderssons@hotmail.com>
2026-02-03 09:41:54 +0100

    Added UserService

--- appstore-api/src/main/java/org/Grupp3Api/Api/Services/UserService.java
+++ appstore-api/src/main/java/org/Grupp3Api/Api/Services/UserService.java
@@ -1,5 +1,51 @@
 package org.Grupp3Api.Api.Services;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.Grupp3Api.Api.Entity.User;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+
+
+@Transactional(Transactional.TxType.SUPPORTS)
+@ApplicationScoped
 public class UserService {
     
+    @Inject
+    EntityManager em;
+
+    UUID uuid;
+
+    @Transactional(Transactional.TxType.REQUIRED)
+    public User registerUser(User user){
+        user.setApiKey(UUID.randomUUID());
+        em.persist(user);
+        return user;
+    }
+    
+        @Transactional(Transactional.TxType.REQUIRED)
+    public User getUserById(Long id){
+
+        return em.find(User.class, id);
+    }
+
+        @Transactional(Transactional.TxType.REQUIRED)
+    public User generateNewApiKey(Long id){
+
+        User user = em.find(User.class, id);
+        user.setApiKey(UUID.randomUUID());
+        return user;
+    }
+
+    public List<User> findAll(){
+
+        List<User> users = em.createQuery("SELECT u FROM User u", User.class).getResultList();
+
+        return users;
+    }
+
 }

8f502a36d57b055d218d4c3f34eab42b3a97a9ad
ninos11 <ninos.nina@hotmail.com>
2026-02-03 12:42:43 +0100

    uppdatera bild och beskrivning

--- appstore-api/src/main/java/org/Grupp3Api/Api/Entity/AppDTO.java
+++ appstore-api/src/main/java/org/Grupp3Api/Api/Entity/AppDTO.java
@@ -3,6 +3,25 @@ package org.Grupp3Api.Api.Entity;
 public class AppDTO {
     
     private String appVersion;
+    private String appDescription;
+    private String appImage;
+
+
+    public String getAppDescription() {
+        return appDescription;
+    }
+
+    public void setAppDescription(String appDescription) {
+        this.appDescription = appDescription;
+    }
+
+    public String getAppImage() {
+        return appImage;
+    }
+
+    public void setAppImage(String appImage) {
+        this.appImage = appImage;
+    }
 
     public String getAppVersion() {
         return appVersion;

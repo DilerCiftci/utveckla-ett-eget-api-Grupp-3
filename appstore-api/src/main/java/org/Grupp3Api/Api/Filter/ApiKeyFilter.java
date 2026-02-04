6d69b87eb0554d14b66be8011084ac69a79643b8
Jonatan Andersson <Jonatan.anderssons@hotmail.com>
2026-02-04 10:18:36 +0100

    Added comments and source in apikeyfilter

--- appstore-api/src/main/java/org/Grupp3Api/Api/Filter/ApiKeyFilter.java
+++ appstore-api/src/main/java/org/Grupp3Api/Api/Filter/ApiKeyFilter.java
@@ -10,6 +10,9 @@ import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 
+//Källa:
+//https://stackoverflow.com/questions/27137593/authenticating-with-an-api-key-in-jax-rs
+
 @Provider
 @Priority(Priorities.AUTHENTICATION)
 public class ApiKeyFilter implements ContainerRequestFilter {
@@ -21,15 +24,18 @@ public class ApiKeyFilter implements ContainerRequestFilter {
     @Override
     public void filter(ContainerRequestContext requestContext) {
         
-
+        //Gets the information stored in x-api-key header and saves it.
         String apiKey = requestContext.getHeaderString(API_KEY_HEADER);
 
         String path = requestContext.getUriInfo().getPath();
 
+        //Checking if the path starts with /public, if it is it will allow access without
+        //forcing a apikey.
         if(path.startsWith("/public")){
             return;
         }
-
+        //Checking if the apikey is null or if it doesnt contains in apikeylist
+        // if any statement is true, aborts connection with 401 Unautherized.
         if (apiKey == null || !userService.getApiKeyList().contains(apiKey)) {
             requestContext.abortWith(
                     Response.status(Response.Status.UNAUTHORIZED)
